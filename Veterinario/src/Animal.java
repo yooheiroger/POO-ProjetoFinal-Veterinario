dde33bb3d076692c50258b53d768f07ca8a76be1
Yoohei <yoohei.haga@edu.unifil.br>
2024-11-14 19:48:55 -0300

    codigo em desenvolvimento

--- Veterinario/src/Animal.java
+++ Veterinario/src/Animal.java
@@ -1,19 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animal {
     private String nome;
     private String tipo;
     private String raca;
     private int idade;
     private String estadoSaude;
+    private String nomeDono;
 
-    public Animal(String nome, String tipo, String raca, int idade, String estadoSaude){
+    public Animal(String nome, String tipo, String raca, int idade, String estadoSaude, String nomeDono){
         this.nome = nome;
         this.tipo = tipo;
         this.raca = raca;
         this.idade = idade;
         this.estadoSaude = estadoSaude;
+        this.nomeDono = nomeDono;
 
     }
-    public Animal(){
+//    public Animal(){
+//
+//    }
+
+    public List<Animal> cadastroAnimal( Animal animal1){
+        List<Animal> listaAnimal = new ArrayList<>();
+        listaAnimal.add(animal1);
+
+        return listaAnimal;
 
     }
 
@@ -30,5 +43,43 @@ public class Animal {
 
     }
 
+    public String getEstadoSaude() {
+        return estadoSaude;
+    }
 
+    public void setEstadoSaude(String estadoSaude) {
+        this.estadoSaude = estadoSaude;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- Veterinario/src/Main.java
+++ Veterinario/src/Main.java
@@ -1,9 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, escolha uma das opções abaixo: ");
-        System.out.println("1.Cadastrar cliente 2.Cadastrar Animal 3.Agendar consulta " +
-                "4.Gerar solicitação de exame 5.Gerar receita médica");
+        Scanner scanner = new Scanner(System.in);
 
-        
+
+        // menu do sistema do veterianario
+
+        System.out.println("Olá, escolha uma das opções abaixo: \n");
+        System.out.println("1.Cadastrar cliente \n" +
+                "2.Cadastrar Animal \n" +
+                "3.Agendar consulta \n" +
+                "4.Gerar solicitação de exame \n" +
+                "5.Gerar receita médica\n" +
+                "6.Consultar animal\n");
+        ;
+
+        int opcao = scanner.nextInt();
+
+        switch (opcao){
+            case 1:
+
+
+            case 2:
+                System.out.println("Cadastrar Animal");
+
+                System.out.println("Nome do animal");
+                String nomeAnimal = scanner.nextLine();
+
+                System.out.println("idade do animal");
+                int idadeAnimal = scanner.nextInt();
+
+                System.out.println("Tipo do animal");
+                String tipo = scanner.nextLine();
+
+                System.out.println("Raça do animal");
+                String raca = scanner.nextLine();
+
+                System.out.println("Estado de saude do animal");
+                String estadoSaude = scanner.nextLine();
+
+                System.out.println("Digite o nome do Dono do Animal");
+                String nomeDonoAnimal = scanner.nextLine();
+
+                Animal animal1 = new Animal(nomeAnimal,tipo, raca,
+                        idadeAnimal, estadoSaude, nomeDonoAnimal);
+
+
+
+//                animal1.setNome(nomeAnimal);
+//                animal1.setIdade(idadeAnimal);
+
+//                List<Animal> listaAnimal = new ArrayList<>();
+//                listaAnimal.add(animal1);
+
+                animal1.cadastroAnimal(animal1);
+
+            case 3:
+                System.out.println("Agendar consulta");
+                System.out.println("Informe a data da consulta: ");
+                String data = scanner.nextLine();
+                System.out.println("Informe o convênio: ");
+                String convenio = scanner.nextLine();
+                System.out.println("Informe a duração da consulta: ");
+                int duracao = scanner.nextInt();
+                System.out.println("Informe o valor da consulta: ");
+                int valor = 120 * (duracao/60);
+
+        }
+
+
+
     }
 }
